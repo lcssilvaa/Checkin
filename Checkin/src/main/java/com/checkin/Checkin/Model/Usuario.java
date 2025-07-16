fc02726d67a2aebb97a5e74b2c8f0c9a1d621bdf
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-07-15 22:00:39 -0300

    feat: user model

---
+++ Checkin/src/main/java/com/checkin/Checkin/Model/Usuario.java
@@ -0,0 +1,24 @@
+package com.checkin.Checkin.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    private String usuario;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(name = "filial_sigla")
+    private String filialSigla;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_usuario")
+    private TipoUsuario tipoUsuario;
+}
+
---
+++ Checkin/src/test/java/com/checkin/Checkin/CheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkin.Checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
