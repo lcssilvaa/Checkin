709e1f74ce9e05d2e9eb6575e83d19ce09e29b34
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-07-15 23:01:06 -0300

    feat: user repository

---
+++ Checkin/src/main/java/com/checkin/Checkin/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.checkin.Checkin.repository;
+
+import com.checkin.Checkin.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+        Usuario findByUsuario(String usuario);
+    }
+
