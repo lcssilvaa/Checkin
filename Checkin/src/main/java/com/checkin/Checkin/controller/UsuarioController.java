17a4efa7e773db833f9b54d9126930f8384400c9
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-07-19 00:47:41 -0300

    feat: métodos crud completo para usuário

--- Checkin/src/main/java/com/checkin/Checkin/controller/UsuarioController.java
+++ Checkin/src/main/java/com/checkin/Checkin/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package com.checkin.Checkin.controller;
 
+import java.util.List;
 import com.checkin.Checkin.model.Usuario;
 import com.checkin.Checkin.services.UsuarioService;
+
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,15 +21,56 @@ public class UsuarioController {
 
     @PostMapping
     public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+        try {
         Usuario salvo = usuarioService.salvarUsuario(usuario);
         return ResponseEntity.ok(salvo);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/{usuario}")
     public ResponseEntity<Usuario> buscarUsuario(@PathVariable String usuario) {
+        try {
         Usuario encontrado = usuarioService.buscarPorUsuario(usuario);
-        if (encontrado == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(encontrado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
-}
+
+    @PutMapping("/{usuario:.+}")
+    public ResponseEntity<Usuario> editarUsuario(@PathVariable String usuario, @RequestBody Usuario usuarioAtualizado) {
+        try {
+        usuarioAtualizado.setUsuario(usuario);
+        Usuario editado = usuarioService.editarUsuario(usuarioAtualizado);
+        return ResponseEntity.ok(editado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @Transactional
+    @DeleteMapping("/{usuario:.+}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable String usuario) {
+        try {
+        usuarioService.deletarUsuario(usuario);
+        return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarTodosUsuarios() {
+        try {
+        List<Usuario> usuarios = usuarioService.listarTodosUsuarios();
+        return ResponseEntity.ok(usuarios);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+} 
+
+
 
